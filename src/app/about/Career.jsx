6ae4eaf8f1f8562dfce3fec4e92e61b1b2120a31
Nivesh Rohankar <niveshrohankar@gmail.com>
2023-11-07 16:18:48 +0530

    final commit

--- src/app/about/Career.jsx
+++ src/app/about/Career.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Career() {
   return (
-    <div className='bg-[#f0f0f0] text-[#131313] py-8 px-6'>
+    <div className='bg-[#f0f0f0] text-[#131313] py-8 px-6 md:px-96'>
         <h3 className='text-3xl font-semibold'>CAREER</h3>
         <h1 className='text-4xl md:text-5xl font-bold'>Current Openings</h1>
-          <p className='py-4'>“If you believe your skills align with our team’s vision, we invite you to get in touch with us. Should you feel you are a suitable candidate, kindly forward your email to <span className='font-semibold'>sidegigsdesignlab@gmail</span> come along with your comprehensive portfolio and professional experience. <br />
+          <p className='py-4'>“If you believe your skills align with our team&apos;s vision, we invite you to get in touch with us. Should you feel you are a suitable candidate, kindly forward your email to <span className='font-semibold'>sidegigsdesignlab@gmail</span> come along with your comprehensive portfolio and professional experience. <br />
           We are eager to learn more about your motivations for collaboration and the value you can bring to our projects.”</p>
     </div>
   )
