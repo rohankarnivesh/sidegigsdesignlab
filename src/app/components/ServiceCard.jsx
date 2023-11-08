087b1b3a1ca9532ebd8ffbbca18f1682c8b6229f
Nivesh Rohankar <niveshrohankar@gmail.com>
2023-11-08 17:10:19 +0530

    q

--- src/app/components/ServiceCard.jsx
+++ src/app/components/ServiceCard.jsx
@@ -8,7 +8,7 @@ export default function ServiceCard({iconUrl, title, demo}) {
               <h3 className='text-xl lg:text-2xl mt-6'>{title}</h3>
           </div>
           <div className=' cursor-pointer'>
-              <h2 className='px-10 bg-[#131313] hover:bg-[#CEF62E] transition-all ease-in-out hover:text-black border rounded-lg text-lg py-2 mt-4'>
+              <h2 className='px-10 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-700 hover:bg-gradient-to-r hover:from-[#CEF62E] hover:to-[#CEF62E] transition-all ease-in-out hover:text-black border rounded-lg text-lg py-2 mt-4'>
                 {demo}
                <Image src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='white' viewBox='0 0 24 24'%3E%3Cpath d='M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z'%3E%3C/path%3E%3C/svg%3E" 
                width={24} 
